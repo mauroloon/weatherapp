8fb5adf047bdbb69dece8d2e13edd76bdda6067e
Mauricio Quezada García <m.quezadag@utem.cl>
2018-08-24 16:15:42 -0300

    avance 24-08-18 16:15

--- wheather-app/src/components/WeatherTemperature.js
+++ wheather-app/src/components/WeatherTemperature.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import WeatherIcon from 'react-weathericons';
 
-const WeatherTemperature = () => (
-    <div><span>25°</span></div>
+
+const getweatherIcon = weatherState =>{
+    switch (weatherState) {
+        case 'cloud':
+            return (<WeatherIcon name="cloud" size="2x"/>);
+        default:
+            return (<WeatherIcon name="sleet" size="2x"/>);
+    }
+};
+
+const WeatherTemperature = ({ temperature, weatherState}) => (
+    <div>
+        { getweatherIcon(weatherState) }
+        <span>
+            { `${temperature} C°` }
+        </span>
+    </div>
 );
 
 export default WeatherTemperature;
