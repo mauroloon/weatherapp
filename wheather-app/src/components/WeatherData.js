c55673e9633f0d73f6d6d9b090e355dcadfac70e
Mauro <mauricio.quezadagarcia@gmail.com>
2018-08-26 00:45:42 -0300

    proptypes validacion de campos

--- wheather-app/src/components/WeatherData.js
+++ wheather-app/src/components/WeatherData.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import WeatherExtraInfo from './WeatherExtraInfo';
 import WeatherTemperature from './WeatherTemperature';
+import { 
+    WINDY} from './../constants/weather';
 
 const WeatherData = () => (
     <div>
-        <WeatherTemperature temperature={20}/>
+        <WeatherTemperature temperature={20} weatherState={WINDY}/>
         <WeatherExtraInfo humidity ={80} wind={"10 m/s"}/>
     </div>
 );
--- wheather-app/src/components/WeatherTemperature.js
+++ wheather-app/src/components/WeatherTemperature.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import WeatherIcon from 'react-weathericons';
+import PropTypes from 'prop-types';
+import { CLOUD, 
+    CLOUDY, 
+    SUN, 
+    RAIN, 
+    SNOW, 
+    WINDY} from './../constants/weather';
 
+
+const stateToIconNmae = weatherState =>{
+    switch(weatherState){
+        case CLOUD:
+            return "cloud";
+        case CLOUDY:
+            return "cloudy";
+        case SUN:
+            return "day-sunny";
+        case RAIN:
+            return "rain";
+        case SNOW:
+            return "snow";
+        case WINDY:
+            return "windy";
+        default:
+            return "day-sunny";
+    }
+};
 
 const getweatherIcon = weatherState =>{
-    switch (weatherState) {
-        case 'cloud':
-            return (<WeatherIcon name="cloud" size="2x"/>);
-        default:
-            return (<WeatherIcon name="sleet" size="2x"/>);
-    }
+    return (<WeatherIcon name={stateToIconNmae(weatherState)} size="2x"/>);
 };
 
 const WeatherTemperature = ({ temperature, weatherState}) => (
@@ -20,4 +41,9 @@ const WeatherTemperature = ({ temperature, weatherState}) => (
     </div>
 );
 
+//para validar los datos
+WeatherTemperature.propTypes = {
+    temperature: PropTypes.number.isRequired,
+    weatherState: PropTypes.string,
+};
 export default WeatherTemperature;
