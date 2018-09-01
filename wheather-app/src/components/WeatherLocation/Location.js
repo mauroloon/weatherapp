094d7af595a492c2579fd98c5d2f4b08a034dc65
Mauro <mauricio.quezadagarcia@gmail.com>
2018-09-01 16:47:21 -0300

    estilo box css

--- wheather-app/src/components/WeatherLocation/Location.js
+++ wheather-app/src/components/WeatherLocation/Location.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './styles.css';
 
 const Location = ({ city }) => {
     // Destructuring
     //no es necesario hacer props.city si city esta dentro de { }
     //const { city } = props;
     return (
-        <div>
+        <div className="LocationCont">
             <h1>
                 { city }
             </h1>
