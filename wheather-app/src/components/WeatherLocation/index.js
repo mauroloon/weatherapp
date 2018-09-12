64fe7b492f600eebd44235b54cb2fdba77bdb242
Mauro <mauricio.quezadagarcia@gmail.com>
2018-09-11 22:18:21 -0300

    shorhand typescript y convertir temperatura

--- wheather-app/src/components/WeatherLocation/index.js
+++ wheather-app/src/components/WeatherLocation/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import convert from 'convert-units';
 import Location from './Location';
 import WeatherData from './WeatherData';
 import { SUN } from './../../constants/weather';
@@ -8,8 +9,8 @@ const location = "Santiago";
 const api_key = "0b2796afb46e67815b2b056c9ecf2f20";
 const api_weather = `http://api.openweathermap.org/data/2.5/weather?q=${ location }&appid=${ api_key }`;
 
-const data1 = {
-        temperature: 20,
+const data = {
+        temperature: 25,
         weatherState: SUN,
         humidity: 10,
         wind: '10 m/s',
@@ -22,17 +23,43 @@ class WeatherLocation extends Component {
         super();
         this.state = { //se asigna
             city: 'Santiago',
-            data: data1
+            data: data
         };
+    }
+
+    getTemp = kelvin =>{
+        return Number(convert(kelvin).from("K").to("C").toFixed(2));
+    }
+
+    getWeatherState = weather_data =>{
+        return SUN;
+    }
+
+    getData = weather_data => {
+        const { humidity, temp } = weather_data.main;
+        const { speed } = weather_data.wind;
+        const weatherState = this.getWeatherState(weatherState);
+        const temperature = this.getTemp(temp);
+        const data = {
+            humidity,
+            temperature, //cuando se setea si esta escrito igual no es necesario setear
+            weatherState,
+            wind: `${speed} m/s`,
+        }
+
+        return data;
     }
 
     handleUpdateClick = () => {
         fetch(api_weather).then( data => {
             console.log(data);
             return data.json();
-        }).then( weather_data =>{
-            debugger;
-            console.log(weather_data);
+        }).then( data =>{
+            const newWeather = this.getData(data);
+            console.log(newWeather);
+            this.setState({
+                data : newWeather
+            });
         });
     }
 
