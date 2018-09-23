b6af7ba543dca7b5683b9898e9b85f07a29307e8
Mauro <mauricio.quezadagarcia@gmail.com>
2018-09-23 20:46:27 -0300

    mapeo de ciudades

--- wheather-app/src/App.js
+++ wheather-app/src/App.js
@@ -2,11 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 import LocationList from './components/LocationList';
 
+
+const cities = [
+  'Santiago',
+  'Madrid',
+  'Bogota',
+  'Londres',
+  'Lima',
+]
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <LocationList />
+        <LocationList cities={cities} />
       </div>
     );
   }
--- wheather-app/src/components/LocationList.js
+++ wheather-app/src/components/LocationList.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import WeatherLocation from './WeatherLocation';
 
-const LocationList = () => (
-    <div>
-        <WeatherLocation city="Santiago"/>
-        <WeatherLocation city="Washington"/>
-        <WeatherLocation city="Londres"/>
-    </div>
-)
+const strToComponents = cities => (
+    cities.map( city  => <WeatherLocation key={city} city={city} />)
+);
 
+const LocationList = ({ cities }) => (
+    <div>
+        {strToComponents(cities)}
+    </div>
+);
+
+
+LocationList.propTypes = {
+    cities: PropTypes.array.isRequired,
+}
 export default LocationList;
