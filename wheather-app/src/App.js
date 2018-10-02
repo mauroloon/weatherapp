425050b6c3dddc12451ce7466a1d4ba9bcbd15b4
Mauro <mauricio.quezadagarcia@gmail.com>
2018-10-01 22:49:23 -0300

    aplicación de react-flexbox

--- wheather-app/src/App.js
+++ wheather-app/src/App.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import Paper from '@material-ui/core/Paper';
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
+import Toolbar from '@material-ui/core/Toolbar';
+import { Grid, Row, Col} from 'react-flexbox-grid';
 import './App.css';
 import LocationList from './components/LocationList';
 
@@ -11,6 +16,11 @@ const cities = [
   'Lima',
 ]
 
+/* xs = pantalla chica, sm = pantalla Media, md = pantalla grande 
+  fluid = para aprovechar la pantalla
+  si se deja el xs solo es un autosize, no se bajan las columnas
+*/
+
 class App extends Component {
 
   handleSelectionLocation = city => {
@@ -18,10 +28,30 @@ class App extends Component {
   }
   render() {
     return (
-      <div className="App">
-        <LocationList cities={cities} 
-          onSelectedLocation = {this.handleSelectionLocation}/>
-      </div>
+      <Grid fluid> 
+        <Row>
+          <AppBar position='sticky'>
+            <Toolbar>
+              <Typography variant='title' color='inherit'>
+                Weather App
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        </Row>
+        <Row>
+          <Col xs={12} md={6}>
+            <LocationList 
+              cities={cities} 
+              onSelectedLocation = {this.handleSelectionLocation}>
+            </LocationList>
+          </Col>  
+          <Col xs={12} md={6}>
+            <Paper elevation={4}>
+              <div className="details"></div>
+            </Paper>
+          </Col>
+        </Row>
+      </Grid>
     );
   }
 }
